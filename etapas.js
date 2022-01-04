cf2f67a151f5c45014ad6ed7998bbbeccf035c7a
gilmar jose <gilmarjose2014@gmail.com>
2022-01-03 22:47:14 -0300

    arquivo etapa.js e imagens

---
+++ etapas.js
@@ -0,0 +1,61 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: 38111,
+                nome: 'Biu Roseno',
+                partido: 'ABC',
+                fotos: [{
+                    url: '38111.jpg',
+                    legenda: 'vereador'
+                }]
+            },
+            {
+                numero: 48222,
+                nome: 'Jasanã dourado',
+                partido: 'DEF',
+                fotos: [{
+                    url: '48222.jpg',
+                    legenda: 'vereador'
+                }]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: 99,
+                nome: 'Preferido do Povo',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [{
+                        url: '99.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '99_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+            {
+                numero: 49,
+                nome: 'Zeca Tatu',
+                partido: 'DEF',
+                vice: 'Zul',
+                fotos: [{
+                        url: '84.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '84_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            }
+        ]
+    }
+]
